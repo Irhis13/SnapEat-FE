4839ac2e4669096126e6178f188e4be3a00b697a
rover <110656154+Irhis13@users.noreply.github.com>
2025-05-03 01:40:05 +0200

    Caroussel de imagenes para el header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  images = [
+    { url: 'assets/images/caroussel1.jpg', tipo: 'comida' },
+    { url: 'assets/images/caroussel2.jpeg', tipo: 'postre' },
+    { url: 'assets/images/caroussel3.JPG', tipo: 'empanada' },
+    { url: 'assets/images/caroussel4.avif', tipo: 'vegetariano'}
+  ];
 
+  currentIndex = 0;
+
+  constructor(private router: Router) {
+    setInterval(() => this.nextImage(), 5000);
+  }
+
+  goToTipo(tipo: string): void {
+    this.router.navigate(['/recetas'], { queryParams: { tipo } });
+  }
+
+  nextImage(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevImage(): void {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
 }
